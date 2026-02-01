bd823c03b94cc72c964592628ec5a236fd3073bc
Venu4592 <velankanni4592@gmail.com>
2026-02-01 11:12:52 +0530

    Test Leaf Assignment 1

---
+++ UsingVariables.js
@@ -0,0 +1,11 @@
+let firstName="Velankanni";
+let companyName="abc.com";
+let mobileNo=123456789n
+let isAutomation=true;
+let hasPlaywright;
+
+console.log(firstName +" "+typeof(firstName));
+console.log(companyName+" "+typeof(companyName))
+console.log(mobileNo+" "+typeof(mobileNo))
+console.log(isAutomation+" "+typeof(isAutomation));
+console.log(hasPlaywright+" "+typeof(hasPlaywright));
