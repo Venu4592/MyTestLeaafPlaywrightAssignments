6e8ad59cc9819a641242eefe30c13f2e293f145b
Venu4592 <98342000+Venu4592@users.noreply.github.com>
2026-02-08 12:16:21 +0530

    Home Assignment 07 Feb 2026
    Call Back Home Assignment

---
+++ Callback.js
@@ -0,0 +1,15 @@
+let browser="chrome"
+// Function 1 checkBrowserVersion
+function checkBrowserVersion(callback){
+    console.log("Browser Version is ...")
+    // Setting the time out to load the version for 2 sec
+    setTimeout(function(){
+        callback(browser);
+    },2000);
+}
+// Function 2 logBrowserVersion
+function logBrowserVersion(browserName){
+    console.log("Browser Version: ",browserName)
+}
+
+checkBrowserVersion(logBrowserVersion)
